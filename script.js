a3da1b4a3e2e9ba4f9d8283782b8b5aa37b508ad
Brenda <brendakajuju777@gmail.com>
2023-03-17 15:52:13 +0300

    Rough draft

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const h2 = document.createElement ("h2");
-h2.textContent = "This content added by Javascript"
-document.querySelector("body").appendChild(h2);
+// This function will scroll to the specified section when a navigation link is clicked
+function scrollToSection(sectionId) {
+    const section = document.getElementById(sectionId);
+    section.scrollIntoView({ behavior: 'smooth' });
+  }
+  
+  // This code adds an event listener to each navigation link
+  const navLinks = document.querySelectorAll('nav ul li a');
+  navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+      event.preventDefault();
+      const sectionId = link.getAttribute('href').substring(1);
+      scrollToSection(sectionId);
+    });
+  });
+  
